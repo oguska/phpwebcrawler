1dca9a0ae74c3b4934b82c37260eff61d3e3feb4
Oguska <powerlogy@gmail.com>
2015-09-25 23:52:44 +0300

    Link name update
    Getting links attributes as plaintext.

--- crawler.php
+++ crawler.php
@@ -36,10 +36,11 @@ function crawl_site($u){
   $crawled_urls[$uen]=date("YmdHis");
   foreach($html->find("a") as $li){
    $url=perfect_url($li->href,$u);
+   $title = $li->plaintext;
    $enurl=urlencode($url);
    if($url!='' && substr($url,0,4)!="mail" && substr($url,0,4)!="java" && array_key_exists($enurl,$found_urls)==0){
     $found_urls[$enurl]=1;
-    echo "<li><a target='_blank' href='".$url."'>".$url."</a></li>";
+    echo "<li><a target='_blank' href='".$url."'>".$title."</a></li>";
    }
   }
  }
